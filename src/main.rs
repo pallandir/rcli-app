51ad5406c5193e1cb7722a5b808ae73a1a7284ce
pallandir <pallandir2@gmail.com>
2025-06-22 22:36:22 +0800

    Add cli args struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+struct CliArgs {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
     let pattern = std::env::args()
         .nth(1)
         .expect("No argument provided.\n --help for usage.");
     let path = std::env::args().nth(2).expect("No file path given.");
-    println!("Pattern: {:?}, Path: {:?}", pattern, path);
+    let args = CliArgs {
+        pattern,
+        path: std::path::PathBuf::from(path),
+    };
+    println!("Pattern: {:?}, Path: {:?}", args.pattern, args.path);
 }
