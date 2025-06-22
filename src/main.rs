b668b8c80bb292f2b6c162952254c5ee020c17ca
pallandir <pallandir2@gmail.com>
2025-06-22 20:04:28 +0800

    Get arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args()
+        .nth(1)
+        .expect("No argument provided.\n --help for usage.");
+    let path = std::env::args().nth(2).expect("No file path given.");
+    println!("Pattern: {:?}, Path: {:?}", pattern, path);
 }
