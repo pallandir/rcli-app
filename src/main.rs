c40ddc4d07d541806710ca90aa10292da6ff0aa5
pallandir <pallandir2@gmail.com>
2025-06-22 22:42:37 +0800

    Refractor to integrate with clap parser

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser)]
 struct CliArgs {
     pattern: String,
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args()
-        .nth(1)
-        .expect("No argument provided.\n --help for usage.");
-    let path = std::env::args().nth(2).expect("No file path given.");
-    let args = CliArgs {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = CliArgs::parse();
     println!("Pattern: {:?}, Path: {:?}", args.pattern, args.path);
 }
