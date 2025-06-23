26748e080cc7eda7d1fc5ae2efa13c62b64faa59
pallandir <pallandir2@gmail.com>
2025-06-23 21:22:42 +0800

    Add file reader with BufReader

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 
 #[derive(Parser)]
 struct CliArgs {
@@ -6,7 +9,14 @@ struct CliArgs {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args = CliArgs::parse();
+    let file = File::open(&args.path)?;
+    let reader = BufReader::new(file);
     println!("Pattern: {:?}, Path: {:?}", args.pattern, args.path);
+
+    for line in reader.lines() {
+        println!("Line: {:?}", line);
+    }
+    Ok(())
 }
